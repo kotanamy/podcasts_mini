2336f0e87f727448c0cb37a5e7c430cb9e9df233
kotanamy <donat0009@outlook.com>
2022-09-07 17:04:28 +0300

    Init

---
+++ Podcasts_mini/Podcasts_mini/Podcasts_miniApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podcasts_miniApp.swift
+//  Podcasts_mini
+//
+//  Created by Ярослав Котов on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Podcasts_miniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
